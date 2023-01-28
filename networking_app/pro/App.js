dca70ec1f009ad34212c8d636f9aec71194b9ebb
Hamid Raza <74106196+razahamid91@users.noreply.github.com>
2023-01-28 16:57:34 +0530

    React-project

---
+++ networking_app/pro/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AboutUs from './components/AboutUs/AboutUs';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+// import Login from './components/Login/Login';
+
+function App() {
+  return (
+
+    <BrowserRouter>
+
+    <div className='app'>
+      <Header/>
+      <div className='app-body'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about-us' element={<AboutUs />} />
+          <Route path='/profile' element={<h1>Profile Page</h1>} />
+        </Routes>
+
+      </div>
+    </div>
+    
+    
+      </BrowserRouter>
+    
+    
+  );
+}
+
+export default App;
---
+++ networking_app/pro/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css' 
+import Sidebar from './Sidebar/Sidebar';
+
+function Home() {
+    return (
+       <div className='container_home'>
+        <Sidebar/>
+       </div>
+    )
+}
+
+export default Home;
